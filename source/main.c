c5729aae5db8d1d413dd31c60378068c50ed1498
Christopher Sommerauer <somm_chris@gmx.at>
2023-01-07 19:53:10 +0100

    Drehgeber

--- source/main.c
+++ source/main.c
@@ -3,6 +3,7 @@
 //#include "driver/gpio.h"
 
 #include "IO.h"
+#include "RotaryEncoder.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -11,6 +12,7 @@
 #define LED_DIM                 3400
 #define LED_UNDIM               1800
 
+rotary_encoder_t *encoder = NULL;
 
 void Startup()
 {
@@ -22,13 +24,20 @@ void Startup()
     IO_PWMChannels.HoazLED.Soll = 0;
     gpio_set_level(GPIO_LED_RELAIS, 1);
     gpio_set_level(GPIO_HEIZMATTE_RELAIS, 0);
-}
+
+    uint32_t pcnt_unit = 0;
+    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)pcnt_unit, 34, 35);
+    rotary_encoder_new_ec11(&config, &encoder);
+    encoder->set_glitch_filter(encoder, 1);    // Filter out glitch (1us)
+    encoder->start(encoder);   // Start encoder
+ }
 
 
 void app_main(void)
 {
     bool    DimLED = false;
     bool    DimLock = false;
+    int32_t Counter;
 
     Startup();
 
@@ -53,6 +62,7 @@ void app_main(void)
             IO_PWMChannels.LED.Soll = LED_UNDIM;
 
         IO_PWMUpdate();
+        Counter = encoder->get_counter_value(encoder);
         vTaskDelay(10);
     }
 }
